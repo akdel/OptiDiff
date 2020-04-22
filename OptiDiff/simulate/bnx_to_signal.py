1dd80722ca15e650ea622bea6a77c6be5d8df60e
Akdel <mehmet_akdel@hotmail.com>
2020-04-22 13:26:08 +0200

    added files

---
+++ OptiDiff/simulate/bnx_to_signal.py
@@ -0,0 +1,216 @@
+from OptiScan import utils
+import numpy as np
+from scipy import ndimage
+# import matplotlib.pyplot as plt
+from LSH import lsh
+
+
+class BnxToLabels(utils.BnxParser):
+    def __init__(self, bnx_path):
+        utils.BnxParser.__init__(self, bnx_path)
+        self.read_bnx_file()
+
+    def generate_molecule_labels(self):
+        for molecule_id in range(len(self.bnx_arrays)):
+            yield list(self.bnx_arrays[molecule_id]["labels"])
+
+
+class BnxToSignal(utils.BnxParser):
+    def __init__(self, bnx_path, subsample=1):
+        utils.BnxParser.__init__(self, bnx_path)
+        self.read_bnx_file()
+        self.subsample_bnx_arrays(subsample)
+        self.molecule_segment_indices = list()
+        self.molecule_segments = list()
+        self.mol_segment_count = 0
+        self.molecule_lsh = None
+        self.molecule_count = len(self.bnx_arrays)
+        self.segment_densities = list()
+
+    def write_arrays_as_bnx(self, outname):
+        """
+        writes the bnx_arrays into file
+        ie// this is useful if bnx_arrays are modified.
+        """
+        f = open(outname, "w")
+        lines = [str(self.bnx_head_text)]
+        i = 0
+        for arr in self.bnx_arrays:
+            i += 1
+            lines.append("\t".join(list(map(str, arr["info"]))))
+            lines.append("1\t" +  "\t".join([str(x) for x in arr["labels"]]))
+            lines.append("QX11\t" +  "\t".join([str(x) for x in arr["label_snr"]]))
+            lines.append("QX12\t" +  "\t".join([str(x) for x in arr["raw_intensities"]]))
+        f.write("\n".join(lines) + "\n")
+        f.close()
+
+    def subsample_bnx_arrays(self, ratio):
+        if ratio >= 1:
+            self.bnx_arrays = self.bnx_arrays
+        else:
+            samples = np.random.choice(np.arange(len(self.bnx_arrays)), int(len(self.bnx_arrays) * ratio), replace=False)
+            self.bnx_arrays = [self.bnx_arrays[x] for x in samples]
+
+    def create_signal(self, molecule_id):
+        molecule = self.bnx_arrays[molecule_id]
+        indices = (np.array(molecule["labels"])/500.).astype(int)
+        total_length = int(float(molecule["info"][2])/500.)
+        sig = np.zeros(total_length+1, dtype=float)
+        rev_sig = np.zeros(total_length+1, dtype=float)
+        sig[indices] = 5000.
+        rev_sig[np.array([rev_sig.shape[0]-x-1 for x in indices])] = 5000.
+        log_sig = np.log1p(ndimage.gaussian_filter1d(sig, sigma=1))
+        log_rev_sig = np.log1p(ndimage.gaussian_filter1d(rev_sig, sigma=1))
+        self.bnx_arrays[molecule_id]["signal"] = log_sig
+        self.bnx_arrays[molecule_id]["labels"] = indices
+        self.bnx_arrays[molecule_id]["info"][2] = total_length
+        self.bnx_arrays.append({})
+        self.bnx_arrays[-1]["signal"] = log_rev_sig
+        self.bnx_arrays[-1]["labels"] = np.array([rev_sig.shape[0]-x for x in indices])
+
+    def simulate_log_signals(self):
+        for i in range(len(self.bnx_arrays)):
+            bnx_array = self.bnx_arrays[i]
+            if len(bnx_array["labels"]) >=5:
+                self.create_signal(i)
+            else:
+                continue
+
+    def create_mol_segments(self, length=100):
+        filtered_mols = list(filter(lambda x: np.sum(x["signal"]), self.bnx_arrays))
+        self.mol_segment_count = 0
+        cumulative = 0
+        for molecule in filtered_mols:
+            current_segments = [molecule["signal"][i:i+length] for i in molecule["labels"] if molecule["signal"][i:i+length].shape[0] == length]
+            current_density = [len([x for x in molecule["labels"] if i < x < i + length]) for i in molecule["labels"] if molecule["signal"][i:i+length].shape[0] == length]
+            if not len(current_segments):
+                continue
+            self.molecule_segments.append([molecule["signal"][i:i + length] for i in molecule["labels"] if molecule["signal"][i:i + length].shape[0] == length])
+            self.segment_densities.append(current_density)
+            self.molecule_segment_indices.append((cumulative, cumulative + len(self.molecule_segments[-1])))
+            cumulative += len(self.molecule_segments[-1])
+            self.mol_segment_count += len(self.molecule_segments[-1])
+        self.molecule_segments = np.vstack(self.molecule_segments)
+
+    def mol_compress(self, length=100, nbits=16, custom_table=np.array([])):
+        def create_randoms(nbits=nbits, l=length):
+            randoms = np.zeros((nbits, l))
+            steps = l / nbits
+            for i in range(1, nbits):
+                x = np.zeros(l)
+                x[int(i * steps):int(i * steps + steps)] = l
+                randoms[i] = x - np.mean(x)
+            return randoms
+
+        randoms = create_randoms()
+        if custom_table.shape[0]:
+
+            self.molecule_lsh = lsh.VectorsInLSH(nbits, self.molecule_segments, custom_table=custom_table)
+        else:
+            self.molecule_lsh = lsh.VectorsInLSH(nbits, self.molecule_segments, custom_table=randoms)
+
+
+class CmapToLabels(utils.CmapParser):
+    def __init__(self, cmap_path):
+        utils.CmapParser.__init__(self, cmap_path)
+        self.read_and_load_cmap_file()
+        self.get_position_indexes()
+
+    def generate_cmap_labels(self):
+        for chr_id in self.position_index.keys():
+            yield np.unique((np.array(self.position_index[chr_id])).astype(int))
+
+
+class CmapToSignal(utils.CmapParser):
+    def __init__(self, cmap_path):
+        utils.CmapParser.__init__(self, cmap_path)
+        self.read_and_load_cmap_file()
+        self.get_position_indexes()
+        self.simulated_chromosomes = dict()
+        self.chromosome_lsh = None
+        self.chromosome_indices = list()
+        self.chr_segments = list()
+        self.chromosome_segment_indices = list()
+        self.chromosome_segment_density = list()
+
+    def generate_segment_from_bits(self, bits_id):
+        bits = self.chromosome_lsh.search_results[bits_id:bits_id+1].view("uint8")
+        return np.unpackbits(bits).flatten()
+
+    def simulate_chr(self, chr_id, zoom_factor=500):
+        self.position_index[chr_id] = np.unique((np.array(self.position_index[chr_id])//zoom_factor).astype(int))
+        indices = self.position_index[chr_id]
+        arr = np.zeros(np.max(indices[-1])+1, dtype=float)
+        arr[indices] = 5000.
+        self.simulated_chromosomes[chr_id] = np.log1p(ndimage.gaussian_filter1d(arr, sigma=1))
+
+    def simulate_all_chrs(self, zoom_factor=500):
+        for chr_id in self.position_index:
+            self.simulate_chr(chr_id, zoom_factor=zoom_factor)
+
+    def create_chr_segments(self, length=100):
+        self.segment_count = 0
+        cumulative = 0
+        for chr_id, molecule in self.simulated_chromosomes.items():
+            labels = np.array(self.position_index[chr_id])
+            current_segments = [molecule[i:i + length] for i in labels if
+                                molecule[i:i + length].shape[0] == length]
+            if not len(current_segments):
+                continue
+            for j in range(len(labels)):
+                current_density = len([x for x in (labels[j:] - labels[j]) if x < length])
+                self.chromosome_segment_density.append(current_density)
+            self.chr_segments.append([molecule[i:i + length] for i in labels if
+                                      molecule[i:i + length].shape[0] == length])
+            self.chromosome_segment_indices.append((cumulative, cumulative + len(self.chr_segments[-1])))
+            cumulative += len(self.chr_segments[-1])
+            self.segment_count += len(self.chr_segments[-1])
+        self.chr_segments = np.vstack(self.chr_segments)
+
+    def chr_compress(self, nbits=16, length=100, custom_table=np.array([])):
+        def create_randoms(nbits=nbits, l=length):
+            randoms = np.zeros((nbits, l))
+            steps = l / nbits
+            for i in range(1, nbits):
+                x = np.zeros(l)
+                x[int(i * steps):int(i * steps + steps)] = l
+                randoms[i] = x - np.mean(x)
+            return randoms
+
+        randoms = create_randoms()
+        if custom_table.shape[0]:
+
+            self.chromosome_lsh = lsh.VectorsInLSH(nbits, self.chr_segments, custom_table=custom_table)
+        else:
+            self.chromosome_lsh = lsh.VectorsInLSH(nbits, self.chr_segments, custom_table=randoms)
+
+
+def from_fasta(fasta_path, digestion_motif="GCTCTTC", enzyme_name="BSPQ1", channel="1"):
+    temp_name = "temp.cmap"
+    fasta_to_cmap(fasta_path, temp_name, digestion_motif=digestion_motif, enzyme_name=enzyme_name, channel=channel)
+    cmaptosignal = CmapToSignal(temp_name)
+    return cmaptosignal
+
+
+def fasta_to_cmap(fasta_in_path, cmap_out_path, digestion_motif="GCTCTTC",
+                  enzyme_name="BSPQ1", channel="1"):
+    fasta = utils.FastaObject(fasta_in_path)
+    fasta.initiate_fasta_array()
+    fasta.fill_complete_fasta_array()
+    fasta.write_fasta_to_cmap(digestion_sequence=digestion_motif, output_file_name=cmap_out_path,
+                              enzyme_name=enzyme_name, channel=channel)
+
+
+if __name__ == "__main__":
+    bnx = BnxToSignal("/Users/akdel/PycharmProjects/OptiDiff/yeast_output.label_0.1.bnx")
+    bnx.bnx_arrays = bnx.bnx_arrays[:100]
+    bnx.simulate_log_signals()
+    bnx.create_mol_segments()
+    bnx.mol_compress()
+    print(bnx.molecule_lsh.search_results)
+    print(bnx.molecule_lsh.bin_ids_used.shape)
+    # plt.plot(bnx.segments[0])
+    # plt.show()
+    # plt.plot(bnx.segments[-1])
+    # plt.show()
+    # cmap_stuff = from_fasta("/Users/akdel/PycharmProjects/OptiDiff/S288C_reference_sequence_R64-2-1_20150113.fsa")
---
+++ OptiDiff/simulate/svd.py
@@ -0,0 +1,211 @@
+from OptiScan import utils
+import numpy as np
+import numba as nb
+from subprocess import check_call as ck
+
+
+class Fasta(utils.FastaObject):
+    def __init__(self, fasta_path, digestion_sequence="GCTCTTC"):
+        utils.FastaObject.__init__(self, fasta_path)
+        self.initiate_fasta_array()
+        self.fill_complete_fasta_array()
+        self.indices = list()
+        self.get_indices_from_lengths()
+        self.digest_fasta_array(digestion_sequence)
+
+    def get_indices_from_lengths(self):
+        total = 0
+        for i in range(len(self.lengths)):
+            self.indices.append((total, self.lengths[i]+total))
+            total += self.lengths[i]
+
+    def write_single_chr(self, chr_id, fname="temp.fasta"):
+        start, end = self.indices[chr_id]
+        header = f"> {chr_id}\n"
+        fasta_text = "".join(list(self.fasta_array[0][start:end].view("S1").astype("U1"))) + "\n"
+        f = open(fname, "w")
+        f.write(header)
+        f.write(fasta_text)
+        f.close()
+
+    def write_all_chr(self,  fname="temp.fasta", lim=5000000):
+        header = f"> all_chr\n"
+        fasta_text = "".join(list(self.fasta_array[0].view("S1").astype("U1")))[:lim] + "\n"
+        f = open(fname, "w")
+        f.write(header)
+        f.write(fasta_text)
+        f.close()
+
+    def write_fasta_to_cmap(self, digestion_sequence: str, output_file_name: str, enzyme_name="BSP1Q", channel=1):
+        """
+        Writes the sequences into a CMAP file with an in silico digestion by the given digestion sequence.
+        """
+        from OptiScan.utils import CMAP_HEADER
+
+        def gen_line():
+            return '%s\t%s\t%s\t%s\t%s\t%s\t1.0\t1\t1\n'
+
+        digested_array = np.sum(self.fasta_digestion_array, axis=0)
+
+        f = open(output_file_name, 'w')
+        f.write(CMAP_HEADER % (digestion_sequence, enzyme_name))
+
+        for i in range(len(self.lengths)):
+            start = sum(self.lengths[:i])
+            end = sum(self.lengths[:i + 1])
+            _id = i + 1
+            nicking_sites = np.where(digested_array[start:end] > 0.0)[0]
+            if nicking_sites.shape[0] == 0:
+                continue
+            else:
+                length = end - start
+                for j in range(nicking_sites.shape[0]):
+                    line = gen_line() % (_id, length, nicking_sites.shape[0], j + 1, channel, nicking_sites[j])
+                    f.write(line)
+                line = gen_line()
+                line = line % (_id, length, nicking_sites.shape[0], nicking_sites.shape[0] + 1, 0, length)
+                f.write(line)
+        f.close()
+
+
+class SvdFromFastaArray:
+    def __init__(self, fasta_array, digestion_array,
+                 omsim_exec_path, omsim_enzyme_path,
+                 omsim_template_path, cov=50):
+        self.omsim = OmsimWrapper(omsim_exec_path, omsim_enzyme_path, omsim_template_path, cov=cov)
+        digestion_array = np.sum(digestion_array, axis=0)
+        self.digestion_indices = np.where(digestion_array > 0)[0]
+        self.fasta_array = fasta_array[0]
+        self.tracked_changes = list()
+
+    def introduce_deletion(self, start_dist=50000, var=10000):
+        locus = np.random.randint(0, self.fasta_array.shape[0]-start_dist-var)
+        scale = int(np.random.normal(start_dist, var//2, 1))
+        self.fasta_array = np.concatenate((self.fasta_array[:locus], self.fasta_array[locus+scale:]))
+        self.tracked_changes.append((locus, scale, "del"))
+
+    def introduce_deletion_exact(self, start=50000, end=100000):
+        self.fasta_array = np.concatenate((self.fasta_array[:start], self.fasta_array[end:]))
+        self.tracked_changes.append((start, end, "del"))
+
+    def introduce_deletion_label_based(self, number_of_labels=5):
+        if number_of_labels == -1:
+            _from = np.random.randint(0, self.digestion_indices.shape[0]-1)
+            _to = _from + 1
+            _from = self.digestion_indices[_from]
+            _to = _to + self.digestion_indices[_to]
+            _to - np.random.choice(np.arange(_from, _to))
+        else:
+            number_of_labels += 1
+            _from = np.random.randint(0, self.digestion_indices.shape[0] - number_of_labels)
+            _to = _from + number_of_labels
+            _from = self.digestion_indices[_from]
+            _to = _to + self.digestion_indices[_to]
+        self.fasta_array = np.concatenate((self.fasta_array[:_from], self.fasta_array[_to:]))
+        self.tracked_changes.append((_from, _to, "del"))
+
+    def copy_paste(self, length=10000):
+        start = np.random.randint(0, self.fasta_array.shape[0] - length)
+        end = start + length
+        _to = np.random.randint(0, self.fasta_array.shape[0] - length)
+        self.fasta_array = np.concatenate((self.fasta_array[:_to], self.fasta_array[start:end], self.fasta_array[_to:]))
+        self.tracked_changes.append((start, end, _to, "dup"))
+
+    def write_ref(self, fname="temp_ref.fasta"):
+        header = "> ref\n"
+        fasta_text = "".join(list(self.fasta_array.view("S1").astype("U1"))) + "\n"
+        f = open(fname, "w")
+        f.write(header)
+        f.write(fasta_text)
+        f.close()
+        self.omsim.run_simulation_from_fasta(fname)
+
+    def write(self, fname="temp_svd.fasta"):
+        header = "> svd\n"
+        fasta_text = "".join(list(self.fasta_array.view("S1").astype("U1"))) + "\n"
+        f = open(fname, "w")
+        f.write(header)
+        f.write(fasta_text)
+        f.close()
+        self.omsim.run_simulation_from_fasta(fname)
+
+
+class OmsimWrapper:
+    def __init__(self, executable_path, enzymes_path, param_template_path, cov=50):
+        self.cov = cov
+        self.exec = executable_path
+        self.enzymes = enzymes_path
+        self.template = param_template_path
+        self.params = "params.xml"
+
+    def run_simulation_from_fasta(self, fasta_path):
+        template = open(self.template, "r").read()
+        print(template)
+        template = template.replace("{file1}", fasta_path)
+        template = template.replace("{file2}", self.enzymes)
+        template = template.replace("{cov}", str(self.cov))
+        f = open(self.params, "w")
+        f.write(template)
+        f.close()
+        ck(f"python2 {self.exec} {self.params}", shell=True)
+        ck(f"mv yeast_output.label_0.1.bnx {fasta_path}.bnx", shell=True)
+
+
+if __name__ == "__main__":
+    omsim_exec_path = "/Users/akdel/PycharmProjects/omsim/omsim/src/omsim/__main__.py"
+    omsim_param_template_path = "/Users/akdel/PycharmProjects/omsim/test/ecoli/template.xml"
+    omsim_enzyme_path = "/Users/akdel/PycharmProjects/omsim/test/ecoli/enzymes.xml"
+
+    fasta = Fasta("/Users/akdel/PycharmProjects/OptiDiff/S288C_reference_sequence_R64-2-1_20150113.fsa")
+    print(fasta.fasta_array.shape)
+    fasta.write_all_chr(fname="temp.fasta", lim=5000000) # concats chromosomes into a single fasta entry
+    fasta = Fasta("temp.fasta") # reloads fasta
+    print(fasta.fasta_array.shape)
+    fasta.write_fasta_to_cmap(digestion_sequence="GCTCTTC", output_file_name="temp_all_chr.cmap",
+                              enzyme_name="BSPQ1", channel=1) # writes fasta cmap and digests the sequence
+    # for _ in range(50):
+    #     svd_fasta = SvdFromFastaArray(fasta.fasta_array, fasta.fasta_digestion_array, omsim_exec_path, omsim_enzyme_path,
+    #                                   omsim_param_template_path, cov=75)
+    #     svd_fasta.write(fname=f"negatives/{np.random.randint(0, 1000000, 1)}.fasta")
+    # exit()
+
+    # svd_fasta = SvdFromFastaArray(fasta.fasta_array , fasta.fasta_digestion_array, omsim_exec_path, omsim_enzyme_path,
+    #                               omsim_param_template_path, cov=700) # loads the fasta array and digested sequence
+    # svd_fasta.write_ref(fname="all_ref6.fasta")
+    #
+    # fasta = Fasta("/Users/akdel/PycharmProjects/OptiDiff/OptiDiff/simulate/all_ref6.fasta")
+    # print(fasta.fasta_array.shape)
+    # fasta.write_all_chr(fname="temp.fasta")  # concats chromosomes into a single fasta entry
+    fasta = Fasta("temp.fasta")  # reloads fasta
+    print(fasta.fasta_array.shape)
+    fasta.write_fasta_to_cmap(digestion_sequence="GCTCTTC", output_file_name="temp_all_chr.cmap",
+                              enzyme_name="BSPQ1", channel=1)  # writes fasta cmap and digests the sequence
+    print(fasta.fasta_array.shape)
+    svd_fasta = SvdFromFastaArray(fasta.fasta_array, fasta.fasta_digestion_array, omsim_exec_path, omsim_enzyme_path,
+                                  omsim_param_template_path, cov=1800)
+    svd_fasta.write_ref(fname="all_ref_5mb.fasta")
+    # exit()
+    # print(fasta.fasta_array.shape)
+    # svd_fasta = SvdFromFastaArray(fasta.fasta_array, fasta.fasta_digestion_array, omsim_exec_path, omsim_enzyme_path,
+    #                               omsim_param_template_path, cov=600)
+    # svd_fasta.introduce_deletion_exact(1309859, 1317596)
+    # svd_fasta.write_ref(fname="all_ref_fp.fasta")
+    # exit()
+    # svd_fasta = SvdFromFastaArray(fasta.fasta_array, fasta.fasta_digestion_array, omsim_exec_path, omsim_enzyme_path,
+    #                               omsim_param_template_path, cov=600)
+    # print(svd_fasta.fasta_array.shape)
+    # svd_fasta.introduce_deletion_exact(1309859, 1317596)
+    # svd_fasta.write(
+    #     fname=f"deletion_svd_low/temp_svd_del{svd_fasta.tracked_changes[0][0]}-{svd_fasta.tracked_changes[0][1]}-retry-del.fasta")
+    # exit()
+    for _ in range(5):
+        svd_fasta = SvdFromFastaArray(fasta.fasta_array, fasta.fasta_digestion_array, omsim_exec_path, omsim_enzyme_path,
+                                      omsim_param_template_path, cov=400)
+        print(svd_fasta.fasta_array.shape)
+        svd_fasta.copy_paste(length=500000)
+        # svd_fasta.introduce_deletion_label_based(np.random.choice([-1, 0, 1, 2, 3]))
+        # svd_fasta.copy_paste(length=200000)
+        print(svd_fasta.fasta_array.shape)
+        print(svd_fasta.tracked_changes)
+        # svd_fasta.write(fname=f"simulated_svd2/temp_svd_del{svd_fasta.tracked_changes[0][0]}-{svd_fasta.tracked_changes[0][1]}-{svd_fasta.tracked_changes[0][-1]}.fasta")
+        svd_fasta.write(fname=f"simulated_svd/temp_svd_dup{svd_fasta.tracked_changes[0][0]}-{svd_fasta.tracked_changes[0][1]}-{svd_fasta.tracked_changes[0][2]}-{svd_fasta.tracked_changes[0][-1]}.fasta")
