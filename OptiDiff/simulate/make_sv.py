b79f49cd6130a263f8f9a8631593ba268af16a4e
akdel <mehmet_akdel@hotmail.com>
2020-06-18 00:37:00 +0200

    test cont.

--- OptiDiff/simulate/make_sv.py
+++ OptiDiff/simulate/make_sv.py
@@ -28,7 +28,6 @@ def fasta_to_cmap_indices(out_file: str, fasta_file: str, digestion_motif: str =
                    seq_to_cmap_indices(list(reverse_complement(sequence)), digestion_motif))
         return sorted(itertools.chain.from_iterable((forward, reverse)))
 
-    # f = open(fasta_file, "r")
     of = open(out_file, "w")
     _id = 0
     for k, g in itertools.groupby((x for x in open(fasta_file, "r")), lambda x: x.startswith(">")):
@@ -46,7 +45,6 @@ def fasta_to_cmap_indices(out_file: str, fasta_file: str, digestion_motif: str =
             of.write(f"{_id}\t{last_index}\t{len(digestion_indices)}\t{len(digestion_indices) + 1}\t{0}\t{digestion_indices[i]}\t1.0\t1\t1\n")
         else:
             _id += 1
-    # f.close()
     of.close()
 
 
