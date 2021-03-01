473c5194814fda0b10cfc653ef35b6726a5d3434
akdel <mehmet_akdel@hotmail.com>
2021-03-01 22:59:14 +0100

    bugfix

--- OptiDiff/sv_detect.py
+++ OptiDiff/sv_detect.py
@@ -1079,9 +1079,9 @@ def merge_to_heterozygous(reference_bnx_path: str,
     sv_bnx_lines = get_array_dict(sv_bnx)
     for bnx_id in sv_bnx_lines.keys():
         sv_bnx_lines[bnx_id]["info"][1] = str(int(sv_bnx_lines[bnx_id]["info"][1]) + id_shift)
-    sv_bnx.bnx_arrays = ref_lines + list(sv_bnx_lines.values())
+    sv_bnx.bnx_arrays = ref_lines[:-1] + list(sv_bnx_lines.values())[1:]
     if file_write:
-        sv_bnx.write_arrays_as_bnx(f"{sv_bnx_path}.heterozygous")
+        sv_bnx.write_arrays_as_bnx(f"{sv_bnx_path}.heterozygous.bnx")
     return sv_bnx.bnx_arrays
 
 
